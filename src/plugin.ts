b1f8a5cc781b0850df3f92745f14cf6e53186d7f
Alireza <ialirezamhi@gmail.com>
2024-11-25 22:31:14 +0330

    update

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,30 +1,37 @@
-import * as hooks from './hooks';
-
 type HookCallback = (data: any) => Promise<void> | void;
 
 class WebitoPlugin {
-  private name: string;
-  private hooks: Record<string, HookCallback[]>;
+    private name: string;
+    private hooks: Record<string, HookCallback[]> = {};
+    private registeredHooks: Set<string> = new Set();
 
-  constructor(name: string) {
-    this.name = name;
-    this.hooks = {};
-  }
-
-  registerHook(hookName: string, callback: HookCallback) {
-    if (!this.hooks[hookName]) {
-      this.hooks[hookName] = [];
+    constructor(name: string) {
+        this.name = name;
     }
-    this.hooks[hookName].push(callback);
-  }
 
-  async executeHook(hookName: string, data: any) {
-    if (this.hooks[hookName]) {
-      for (const callback of this.hooks[hookName]) {
-        await callback(data);
-      }
+    registerHook(hookName: string, callback: HookCallback) {
+        // جلوگیری از ثبت مجدد هوک
+        if (this.registeredHooks.has(hookName)) {
+            console.warn(`Hook ${hookName} is already registered.`);
+            return;
+        }
+        
+        if (!this.hooks[hookName]) {
+            this.hooks[hookName] = [];
+            this.registeredHooks.add(hookName); // ثبت هوک
+        }
+        this.hooks[hookName].push(callback);
     }
-  }
+
+    async executeHook(hookName: string, data: any) {
+        if (this.hooks[hookName]) {
+            for (const callback of this.hooks[hookName]) {
+                await callback(data);
+            }
+        } else {
+            console.warn(`Hook ${hookName} not found.`);
+        }
+    }
 }
 
 export default WebitoPlugin;
