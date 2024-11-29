f4983020e8ac96dd06062333d586518a0957e4d2
Alireza <ialirezamhi@gmail.com>
2024-11-29 17:29:57 +0330

    new hooks added

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,2 +1,38 @@
 export const messagesCreate = 'messagesCreate';
-export const productsCreate = 'productsCreate';
+
+// payments
+export const paymentsCreate = 'paymentsCreate';
+export const paymentsVerify = 'paymentsVerify';
+
+// delivertypes
+export const delivertypesCalculate = 'delivertypesCalculate';
+
+// packs
+export const packsSend = 'packsSend';
+/*before hooks*/
+export const packsCreate_before = 'packsCreate_before';
+export const packsUpdate_before = 'packsUpdate_before';
+export const packsDelete_before = 'packsDelete_before';
+export const packsGet_before = 'packsGet_before';
+export const packsGetAll_before = 'packsGetAll_before';
+/*after hooks*/
+export const packsCreate_after = 'packsCreate_after';
+export const packsUpdate_after = 'packsUpdate_after';
+export const packsDelete_after = 'packsDelete_after';
+export const packsGet_after = 'packsGet_after';
+export const packsGetAll_after = 'packsGetAll_after';
+
+// products
+
+/*before hooks*/
+export const productsCreate_before = 'productsCreate_before';
+export const productsUpdate_before = 'productsUpdate_before';
+export const productsDelete_before = 'productsDelete_before';
+export const productsGet_before = 'productsGet_before';
+export const productsGetAll_before = 'productsGetAll_before';
+/*after hooks*/
+export const productsCreate_after = 'productsCreate_after';
+export const productsUpdate_after = 'productsUpdate_after';
+export const productsDelete_after = 'productsDelete_after';
+export const productsGet_after = 'productsGet_after';
+export const productsGetAll_after = 'productsGetAll_after';
