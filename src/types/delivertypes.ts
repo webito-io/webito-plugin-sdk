f2e8207f977af5e94390510f490e2db0ee21c3bd
Alireza <ialirezamhi@gmail.com>
2024-11-30 19:39:43 +0330

    update

--- src/types/delivertypes.ts
+++ src/types/delivertypes.ts
@@ -1,11 +1,11 @@
 export type delivertypesCalculate_input = {
     items: Array<{ [key: string]: any }>; // array of physical items in cart
     warehouse: { location: { lat: string | number | undefined, lng: string | number | undefined }, name: string | undefined, details: string | undefined }; // { location : { lat , lng } , name , details }
-    billing?: { [key: string]: any };
+    billing: { [key: string]: any };
 }
 
 export type delivertypesCalculate_output = {
     status: boolean;
     cost: number;
-    currency?: string;
+    currency: string;
 }
