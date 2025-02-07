fb8cb4034f39815c9fbde79d97e8c4f7af379bc9
mohagheghi <ialirezamhi@gmail.com>
2025-02-07 20:14:24 +0330

    hooks added

--- src/hooks.ts
+++ src/hooks.ts
@@ -9,30 +9,128 @@ export const delivertypesCalculate = 'delivertypesCalculate';
 
 // packs
 export const packsSend = 'packsSend';
-/*before hooks*/
 export const packsCreate_before = 'packsCreate_before';
 export const packsUpdate_before = 'packsUpdate_before';
-export const packsDelete_before = 'packsDelete_before';
-export const packsGet_before = 'packsGet_before';
-export const packsGetAll_before = 'packsGetAll_before';
-/*after hooks*/
 export const packsCreate_after = 'packsCreate_after';
 export const packsUpdate_after = 'packsUpdate_after';
 export const packsDelete_after = 'packsDelete_after';
-export const packsGet_after = 'packsGet_after';
-export const packsGetAll_after = 'packsGetAll_after';
 
-// products
 
-/*before hooks*/
+export const tasksCreate_before = 'tasksCreate_before';
+export const tasksUpdate_before = 'tasksUpdate_before';
+export const tasksCreate_after = 'tasksCreate_after';
+export const tasksUpdate_after = 'tasksUpdate_after';
+export const tasksDelete_after = 'tasksDelete_after';
+
+export const boardsCreate_before = 'boardsCreate_before';
+export const boardsUpdate_before = 'boardsUpdate_before';
+export const boardsCreate_after = 'boardsCreate_after';
+export const boardsUpdate_after = 'boardsUpdate_after';
+export const boardsDelete_after = 'boardsDelete_after';
+
 export const productsCreate_before = 'productsCreate_before';
 export const productsUpdate_before = 'productsUpdate_before';
-export const productsDelete_before = 'productsDelete_before';
-export const productsGet_before = 'productsGet_before';
-export const productsGetAll_before = 'productsGetAll_before';
-/*after hooks*/
 export const productsCreate_after = 'productsCreate_after';
 export const productsUpdate_after = 'productsUpdate_after';
 export const productsDelete_after = 'productsDelete_after';
-export const productsGet_after = 'productsGet_after';
-export const productsGetAll_after = 'productsGetAll_after';
+
+export const taxesCreate_before = 'taxesCreate_before';
+export const taxesUpdate_before = 'taxesUpdate_before';
+export const taxesCreate_after = 'taxesCreate_after';
+export const taxesUpdate_after = 'taxesUpdate_after';
+export const taxesDelete_after = 'taxesDelete_after';
+
+export const invoicesGet = 'invoicesGet';
+export const invoicesCreate_before = 'invoicesCreate_before';
+export const invoicesUpdate_before = 'invoicesUpdate_before';
+export const invoicesCreate_after = 'invoicesCreate_after';
+export const invoicesUpdate_after = 'invoicesUpdate_after';
+export const invoicesDelete_after = 'invoicesDelete_after';
+
+export const ordersCreate_before = 'ordersCreate_before';
+export const ordersUpdate_before = 'ordersUpdate_before';
+export const ordersCreate_after = 'ordersCreate_after';
+export const ordersUpdate_after = 'ordersUpdate_after';
+export const ordersDelete_after = 'ordersDelete_after';
+
+
+export const bookingsCreate_before = 'bookingsCreate_before';
+export const bookingsVerify_before = 'bookingsVerify_before';
+export const bookingsUpdate_before = 'bookingsUpdate_before';
+export const bookingsCreate_after = 'bookingsCreate_after';
+export const bookingsVerify_after = 'bookingsVerify_after';
+export const bookingsUpdate_after = 'bookingsUpdate_after';
+export const bookingsDelete_after = 'bookingsDelete_after';
+
+
+export const submissionsCreate_before = 'submissionsCreate_before';
+export const submissionsUpdate_before = 'submissionsUpdate_before';
+export const submissionsCreate_after = 'submissionsCreate_after';
+export const submissionsUpdate_after = 'submissionsUpdate_after';
+export const submissionsDelete_after = 'submissionsDelete_after';
+
+export const categoriesCreate_before = 'categoriesCreate_before';
+export const categoriesUpdate_before = 'categoriesUpdate_before';
+export const categoriesCreate_after = 'categoriesCreate_after';
+export const categoriesUpdate_after = 'categoriesUpdate_after';
+export const categoriesDelete_after = 'categoriesDelete_after';
+
+export const brandsCreate_before = 'brandsCreate_before';
+export const brandsUpdate_before = 'brandsUpdate_before';
+export const brandsCreate_after = 'brandsCreate_after';
+export const brandsUpdate_after = 'brandsUpdate_after';
+export const brandsDelete_after = 'brandsDelete_after';
+
+export const variablesCreate_before = 'variablesCreate_before';
+export const variablesUpdate_before = 'variablesUpdate_before';
+export const variablesCreate_after = 'variablesCreate_after';
+export const variablesUpdate_after = 'variablesUpdate_after';
+export const variablesDelete_after = 'variablesDelete_after';
+
+export const warehousesCreate_before = 'warehousesCreate_before';
+export const warehousesUpdate_before = 'warehousesUpdate_before';
+export const warehousesCreate_after = 'warehousesCreate_after';
+export const warehousesUpdate_after = 'warehousesUpdate_after';
+export const warehousesDelete_after = 'warehousesDelete_after';
+
+export const subscriptionsCreate_before = 'subscriptionsCreate_before';
+export const subscriptionsUpdate_before = 'subscriptionsUpdate_before';
+export const subscriptionsVerify_before = 'subscriptionsVerify_before';
+export const subscriptionsCreate_after = 'subscriptionsCreate_after';
+export const subscriptionsVerify_after = 'subscriptionsVerify_after';
+export const subscriptionsUpdate_after = 'subscriptionsUpdate_after';
+export const subscriptionsDelete_after = 'subscriptionsDelete_after';
+
+export const delivertypesCreate_before = 'delivertypesCreate_before';
+export const delivertypesUpdate_before = 'delivertypesUpdate_before';
+export const delivertypesCreate_after = 'delivertypesCreate_after';
+export const delivertypesUpdate_after = 'delivertypesUpdate_after';
+export const delivertypesDelete_after = 'delivertypesDelete_after';
+
+export const discountsCreate_before = 'discountsCreate_before';
+export const discountsUpdate_before = 'discountsUpdate_before';
+export const discountsCreate_after = 'discountsCreate_after';
+export const discountsUpdate_after = 'discountsUpdate_after';
+export const discountsDelete_after = 'discountsDelete_after';
+
+export const reviewsCreate_before = 'reviewsCreate_before';
+export const reviewsUpdate_before = 'reviewsUpdate_before';
+export const reviewsCreate_after = 'reviewsCreate_after';
+export const reviewsUpdate_after = 'reviewsUpdate_after';
+export const reviewsDelete_after = 'reviewsDelete_after';
+
+
+export const usersRegister_before = 'usersRegister_before';
+export const usersCreate_before = 'usersCreate_before';
+export const usersUpdate_before = 'usersUpdate_before';
+export const usersLogin_after = 'usersLogin_after';
+export const usersCreate_after = 'usersCreate_after';
+export const usersRegister_after = 'usersRegister_after';
+export const usersUpdate_after = 'usersUpdate_after';
+export const usersDelete_after = 'usersDelete_after';
+
+export const addressesCreate_before = 'addressesCreate_before';
+export const addressesUpdate_before = 'addressesUpdate_before';
+export const addressesCreate_after = 'addressesCreate_after';
+export const addressesUpdate_after = 'addressesUpdate_after';
+export const addressesDelete_after = 'addressesDelete_after';
